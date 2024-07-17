fc6c8cb59829e5111a95ddc066404405a5b917aa
Thanh Nhan <npthanhnhan2003@gmail.com>
2024-07-17 22:20:19 +0700

    init project

---
+++ src/main/java/com/coffee/shop/management/CoffeeShopManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.coffee.shop.management;
+
+/**
+ *
+ * @author Nhan
+ */
+public class CoffeeShopManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
